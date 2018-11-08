b9d5a10bab5c7a2c32e67e7699988f05a12ee984
liwei <805717605@qq.com>
2018-11-08 21:23:07 +0800

    修改用外部tomcat

--- src/main/java/com/example/springcloudeurekaserver/SpringcloudEurekaserverApplication.java
+++ src/main/java/com/example/springcloudeurekaserver/SpringcloudEurekaserverApplication.java
@@ -2,14 +2,21 @@ package com.example.springcloudeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 //需要有这个注解启动微服务注册发现
 @EnableEurekaServer
-public class SpringcloudEurekaserverApplication {
+public class SpringcloudEurekaserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudEurekaserverApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringcloudEurekaserverApplication.class);
 	}
 }
